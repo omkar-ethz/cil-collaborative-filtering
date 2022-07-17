2b36e9e0c6b372444d001261245e5aace4c89e33
Andreas Tsouloupas <andreas.tsouloupas@outlook.com>
2022-07-17 22:45:26 +0300

    Improved reg svd + Tidy up methods into classes

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
 
 import pandas as pd
 import numpy as np
@@ -6,18 +8,45 @@ import numpy as np
 NUMBER_OF_USERS = 10000
 NUMBER_OF_MOVIES = 1000
 
-def import_data_to_matrix() -> np.ndarray:
-    # Extract data to row-column format
-    data_pd = pd.read_csv("./data/data_train.csv")
+def split_dataset(data, train_size=0.9, random_state=None):
+    # Split the dataset into train and test
+    train_pd, test_pd = train_test_split(data, train_size=train_size, random_state=random_state)
+    return train_pd, test_pd
+
+def get_rmse_score(predictions, target_values):
+    # test our predictions with the true values
+    test_users, test_items = target_values.nonzero()
+    y_pred = []
+    y_true = []
+    for user, item in zip(test_users, test_items):
+        y_pred.append(predictions[user][item])
+        y_true.append(target_values[user][item])
+    return mean_squared_error(y_pred, y_true, squared=False)
+
+def create_matrix_from_raw(data_pd):
     users, movies = \
         [np.squeeze(arr) for arr in \
         np.split(data_pd.Id.str.extract("r(\d+)_c(\d+)").values.astype(int) - 1, 2, axis=-1)]
     predictions = data_pd.Prediction.values
     # Create data matrix
-    data = np.full((NUMBER_OF_USERS, NUMBER_OF_MOVIES), 0)
+    matrix = np.full((NUMBER_OF_USERS, NUMBER_OF_MOVIES), 0)
     for user , movie , pred in zip(users , movies ,predictions):
-        data[user][movie] = pred
-    return data
+        matrix[user][movie] = pred
+    return matrix
+
+def import_data_to_matrix_split(train_size=0.9, random_state=None) -> np.ndarray:
+    # Extract data to row-column format
+    data_pd = pd.read_csv("./data/data_train.csv")
+    train_pd, test_pd = split_dataset(data_pd, train_size=train_size, random_state=random_state)
+    train_matrix = create_matrix_from_raw(train_pd)
+    test_matrix = create_matrix_from_raw(test_pd)
+    return train_matrix, test_matrix
+
+def import_data_to_matrix() -> np.ndarray:
+    # Extract data to row-column format
+    data_pd = pd.read_csv("./data/data_train.csv")
+    matrix = create_matrix_from_raw(data_pd)
+    return matrix
 
 def extract_submission(matrix: np.ndarray, file: str = "sumbission"):
     # Extract sumbission users and movies
